55bf1ccf4625bb9fe3367cd9efa50f2874968925
anhhao17 <hiimanhhao@gmail.com>
2021-10-21 22:29:10 +0700

    Sum big Int

---
+++ tuan3/BigInt.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		BigInteger a=new BigInteger("123456781119");
+		BigInteger b=new BigInteger("1234567811119");
+		BigInteger c=a.add(b);
+		c.print();
+		
+	}
+
+}
